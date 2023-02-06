99e54da3dfd509a51e5b713464a69dfa869a9faf
GitHub Action <action@github.com>
2023-02-06 08:48:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/wiselderl/segamega/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wiselderl/segamega/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wiselderl.segamega.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wiselderl.segamega.MyBundle
 
 class MyApplicationService {
 
